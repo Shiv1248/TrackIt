90bc4657cb1945ab8a68b200a052f40034d41152
Shivansh Mittal <54306441+Shiv1248@users.noreply.github.com>
2025-06-24 21:34:02 +0530

    Delete track-it directory

--- track-it/src/main/java/com/shivansh/trackIt/repository/InvestmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shivansh.trackIt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shivansh.trackIt.entity.Investment;
-
-public interface InvestmentRepository extends JpaRepository<Investment, Long> {}
--- track-it/src/main/java/com/shivansh/trackIt/repository/SubscriptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shivansh.trackIt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shivansh.trackIt.entity.Subscription;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {}
